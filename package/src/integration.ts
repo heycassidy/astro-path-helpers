ab4753a3dfed73fd393d01a0e296ec891ce125b6
Cassidy Smith <cassidysmith319@gmail.com>
2025-04-06 19:48:50 -0500

    extract code injection into separate inject module

--- package/src/integration.ts
+++ package/src/integration.ts
@@ -1,9 +1,12 @@
-import { writeFileSync } from "node:fs"
 import type { AstroIntegration } from "astro"
 import {
   generatePathHelpers,
   generateTypeDeclarations,
 } from "./core/codegen.ts"
+import {
+  injectPathHelpers,
+  injectPathHelpersTypeDeclarations,
+} from "./core/inject.ts"
 import type { HelperRouteMap, PathHelpersOptions } from "./core/types.js"
 import { isSupportedRoute } from "./core/validation.ts"
 import { setupVirtualModule } from "./vite/plugin.ts"
@@ -36,21 +39,14 @@ export default function pathHelpers(
         }
 
         const code = generatePathHelpers(helperRoutes)
-        const codePath = new URL("generated.ts", helpersDir)
-        writeFileSync(codePath, "", "utf-8")
-        writeFileSync(codePath, code, "utf-8")
-
-        // re-generate types when routes change
         const typeDeclarations = generateTypeDeclarations(helperRoutes)
-        const typeDeclarationsPath = new URL("generated.d.ts", helpersDir)
-        writeFileSync(typeDeclarationsPath, typeDeclarations, "utf-8")
+
+        injectPathHelpers(code, helpersDir)
+        injectPathHelpersTypeDeclarations(typeDeclarations, helpersDir)
       },
       "astro:config:done": ({ injectTypes }) => {
         const typeDeclarations = generateTypeDeclarations(helperRoutes)
-        injectTypes({
-          filename: "generated.d.ts",
-          content: typeDeclarations,
-        })
+        injectPathHelpersTypeDeclarations(typeDeclarations, helpersDir)
       },
     },
   }
